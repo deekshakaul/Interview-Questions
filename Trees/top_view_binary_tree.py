50497f62171461346c61fdd17a998a9c13ba1749
deekshakaul <deekshakaul1@gmail.com>
2017-08-11 17:25:22 +0530

    Update top_view_binary_tree.py

--- Trees/top_view_binary_tree.py
+++ Trees/top_view_binary_tree.py
@@ -1,16 +1,16 @@
 #Program to print the top view of binary tree
 # Use level order traversal and vertical order traversal.
 
-horDist=[]
+horizontal_distance=[]
 
 class node:
   def __init__(self,data=0):
     self.data=data
     self.left=None
     self.right=None
-    self.hd=0
+    self.horizontal_distance=0
     
-def LOT(root):
+def Level_order_traversal(root):
     if root is None:
         return
     queue = []
@@ -25,25 +25,25 @@ def LOT(root):
             queue.append(node.right)
     return res
 
-#HD and pre together calculate and store the horizontal distance of each node
-def HD(root,hd):
+#find_horizontal_distance and traverse together calculate and store the horizontal distance of each node
+def find_horizontal_distance(root,horizontal_distance):
     if root==None:
       return
     else:
-        root.hd=hd
+        root.horizontal_distance=horizontal_distance
         if root.left!=None:
-            HD(root.left,hd-1)
+            find_horizontal_distance(root.left,horizontal_distance-1)
         if root.right!=None:
-            HD(root.right,hd+1)
+            find_horizontal_distance(root.right,horizontal_distance+1)
     
-def pre(root):
+def traverse(root):
   if root==None:
     return 
-  pre(root.left)
-  horDist.append((root.hd,root.data))
-  pre(root.right)
+  traverse(root.left)
+  horizontal_distance.append((root.horizontal_distance,root.data))
+  traverse(root.right)
 
-#tree   
+#driver code 
 bucket=[]
 root=node(1)
 root.left=node(2)
@@ -56,23 +56,23 @@ root.right.right=node(8)
 root.right.right.right=node(9)
 
 #find level order traversal of the tree
-res=LOT(root)
+res=Level_order_traversal(root)
 
 #find horizontal distances of the nodes and store tham in buckets according to 
 #their horizontal distance
 
-HD(root,0)
-pre(root)
-horDist.sort(key=lambda x: x[0])
-pair1=horDist[0]
-pair2=horDist[-1]
+find_horizontal_distance(root,0)
+traverse(root)
+horizontal_distance.sort(key=lambda x: x[0])
+pair1=horizontal_distance[0]
+pair2=horizontal_distance[-1]
 bucketmin=pair1[0]
 bucketmax=pair2[0]
 
 #create bucket
 for i in range(bucketmin,bucketmax+1):
   bucket.append([])
-for i in horDist:
+for i in horizontal_distance:
   bucket[i[0]-bucketmin].append(i[1])
   
 #sort elements in each bucket element such that the node appearing first in level 
