22170737d95ca265d11a1ce10c4e4a158f2e8acb
deekshakaul <deekshakaul1@gmail.com>
2017-08-21 11:28:10 +0530

    Create Find_the_compatibility_difference_between_two_arrays.py

---
+++ practise_geeksforgeeks/Find_the_compatibility_difference_between_two_arrays.py
@@ -0,0 +1,15 @@
+# http://www.geeksforgeeks.org/find-compatibility-difference-two-arrays/
+a=input().strip().split(' ')
+b=input().strip().split(' ')
+n=len(a)
+mismatch=0
+for i in range(n):
+  if  a[i]==b[i]:
+    pass
+  else:
+    j=i
+    while j<b.index(a[i]):
+      a[i],a[i+1]=a[i+1],a[i]
+      mismatch+=1
+      j+=1
+print(mismatch)      
